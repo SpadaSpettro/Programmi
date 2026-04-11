5e0018ccbb8e84d25af9729b3c396e2a1c065379
Daniele <59313524+SpadaSpettro@users.noreply.github.com>
2026-04-11 07:44:01 +0200

    Delete Calcolatrice/calcolatrice-oggetti.py

--- Calcolatrice/calcolatrice-oggetti.py
+++
@@ -1,7 +0,0 @@
-class calcolatrice:
-    def __init__(self):
-        self.numero1 = int(input())
-        self.numero2 = int(input())
-
-variabile=calcolatrice()
-print(variabile.numero1+variabile.numero2)
