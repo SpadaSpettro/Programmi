52dfa6f303b293f8e58b1d3c767fb3542209873e
Daniele <daniele22fiordilino@hotmail.it>
2026-04-11 07:41:23 +0200

    prova gitignore

---
+++ Calcolatrice/calcolatrice-oggetti.py
@@ -0,0 +1,7 @@
+class calcolatrice:
+    def __init__(self):
+        self.numero1 = int(input())
+        self.numero2 = int(input())
+
+variabile=calcolatrice()
+print(variabile.numero1+variabile.numero2)
